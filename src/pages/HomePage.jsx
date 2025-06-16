1eedffbf6b0b384bb0c88a494316ce2585c49b46
RajnishPuri <rajnishpuri15@gmail.com>
2025-06-16 11:49:45 +0530

    Auth Completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
 
 const HomePage = () => {
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        async function getUserDetails() {
+            const response = await axios.get(`http://localhost:3000/api/v1/getUserDetail`, {
+                headers: {
+                    authorization: `Bearer ${token}`
+                }
+            })
+            console.log(response.data);
+        }
+        getUserDetails();
+    }, [])
+
     return (
         <div>HomePage</div>
     )
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect } from 'react'
 import Login from '../components/Login';
 import Signup from '../components/Signup';
+import { useNavigate } from 'react-router-dom';
 
 const Landing = ({ buttonClicked, setButtonClicked }) => {
+    const navigate = useNavigate();
     useEffect(() => {
-        console.log(buttonClicked);
-    }, [buttonClicked])
+        const token = localStorage.getItem("token");
+        if (token) {
+            navigate('/home');
+        }
+    }, [])
     return (
         <div className='text-black'>
             {
